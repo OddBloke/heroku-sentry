f421117f47bdb60abafe29803ff56e0ea284f774
Daniel Watkins <daniel@daniel-watkins.co.uk>
2012-06-30 00:56:57 +0100

    Add Sentry configuration.

---
+++ sentry.conf.py
@@ -0,0 +1,36 @@
+import dj_database_url
+
+import os
+
+CONF_ROOT = os.path.dirname(__file__)
+
+DATABASES = {
+    'default': dj_database_url.config(default="postgres://localhost"),
+}
+
+SENTRY_KEY = 'iPCTIZ6lW3ia/znEZUZ3g3RGC15KwjOmew3RI+vSDjcYHSW93ez9hQ=='
+
+# Set this to false to require authentication
+SENTRY_PUBLIC = False
+
+SENTRY_URL_PREFIX = 'http://floating-rain-7931.herokuapp.com'
+
+SENTRY_WEB_HOST = '0.0.0.0'
+SENTRY_WEB_PORT = os.environ.get('PORT', 80)
+SENTRY_WEB_OPTIONS = {
+    'workers': 3,  # the number of gunicorn workers
+    # 'worker_class': 'gevent',
+}
+
+# Mail server configuration
+
+# For more information check Django's documentation:
+#  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ''
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
